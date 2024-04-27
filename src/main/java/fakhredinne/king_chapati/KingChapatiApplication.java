a1725439c8d13a4fa6ce0f0cdad08792d5718d90
Fakhredinne BEN HADJ ABDALLAH <121528935+Fakhredinne21@users.noreply.github.com>
2024-04-28 00:40:57 +0100

    test

--- src/main/java/fakhredinne/king_chapati/KingChapatiApplication.java
+++ src/main/java/fakhredinne/king_chapati/KingChapatiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KingChapatiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KingChapatiApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(KingChapatiApplication.class, args);
+		System.out.println("It work ya chaima");
 	}
 
 }
