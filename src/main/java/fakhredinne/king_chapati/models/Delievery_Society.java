0081f61c0d52658a57a3e9edca4e3e64c62617e3
Fakhredinne BEN HADJ ABDALLAH <121528935+Fakhredinne21@users.noreply.github.com>
2024-04-28 20:06:33 +0100

    Fakhreddine fix user class

--- src/main/java/fakhredinne/king_chapati/models/Delievery_Society.java
+++ src/main/java/fakhredinne/king_chapati/models/Delievery_Society.java
@@ -7,7 +7,7 @@ import lombok.ToString;
 import java.io.Serializable;
 import java.util.Set;
 @Entity
-@Table( name = "Delievery_Society")
+@Table( name = "DelieverySociety")
 @Getter
 @Setter
 @ToString
--- src/main/java/fakhredinne/king_chapati/models/Meal.java
+++ src/main/java/fakhredinne/king_chapati/models/Meal.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table( name = "Meal")
+@Table( name = "Meals")
 @Getter
 @Setter
 @ToString
