4e1751d65b849f0b15b3a83a85a132337d7c61ea
CHAIMA <saadanachaima@gmail.com>
2024-04-28 19:01:59 +0100

    Chaima added entities: - Client - DeliveryAgent - Restaurant - Meal

---
+++ src/main/java/fakhredinne/king_chapati/models/Meal.java
@@ -0,0 +1,20 @@
+package fakhredinne.king_chapati.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table( name = "Meal")
+@Getter
+@Setter
+@ToString
+public class Meal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_meal")
+    private String name;
+    private String description;
+    private int price;
+}
