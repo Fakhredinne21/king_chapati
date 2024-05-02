7063915ed09fb31e443972158da82ed0cad203dc
Fakhredinne BEN HADJ ABDALLAH <121528935+Fakhredinne21@users.noreply.github.com>
2024-05-02 19:49:41 +0200

    Fakhreddine fixing thymleaf

--- src/main/java/fakhredinne/king_chapati/KingChapatiApplication.java
+++ src/main/java/fakhredinne/king_chapati/KingChapatiApplication.java
@@ -1,9 +1,15 @@
 package fakhredinne.king_chapati;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan
 public class KingChapatiApplication {
 
 	public static void main(String[] args) {SpringApplication.run(KingChapatiApplication.class, args);
