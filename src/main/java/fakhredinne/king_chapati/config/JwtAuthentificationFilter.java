e21b94ade40fe0318a64fffd2d4bc9433862e598
Fakhredinne BEN HADJ ABDALLAH <121528935+Fakhredinne21@users.noreply.github.com>
2024-05-09 00:56:55 +0100

    let's do it

--- src/main/java/fakhredinne/king_chapati/config/JwtAuthentificationFilter.java
+++ src/main/java/fakhredinne/king_chapati/config/JwtAuthentificationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,21 +17,18 @@ public class JwtAuthentificationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-             HttpServletResponse response,
-             FilterChain filterChain
+          @NonNull HttpServletRequest request,
+          @NonNull HttpServletResponse response,
+          @NonNull  FilterChain filterChain
     ) throws ServletException, IOException {
         final String authHeader=request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
-        if(authHeader==null||!authHeader.startsWith("Bearer ")){
+        if(authHeader==null||!authHeader.startsWith("Bearer ")){ // jwt token = bearer token
             filterChain.doFilter(request,response);
-            return;
+            return;     //stop execution
         }
-        jwt=authHeader.substring(7);
-        userEmail=  jwtService.extractUsername(jwt);
-
-
-
+        jwt=authHeader.substring(7); //token extraction from authorization header
+        userEmail=  jwtService.extractUsername(jwt);//extract userEmail using jwt service
     }
 }
--- src/main/java/fakhredinne/king_chapati/config/JwtService.java
+++ src/main/java/fakhredinne/king_chapati/config/JwtService.java
@@ -1,12 +1,40 @@
 package fakhredinne.king_chapati.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.eclipse.angus.mail.util.BASE64DecoderStream;
 import org.springframework.stereotype.Service;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY="3ABD28343723177DE381D177953D1";
+   //Extract Username
     public String extractUsername(String token
     ) {
-
-        return null;
+        return extractClaim(token,Claims::getSubject);
     }
+
+    //Extract One Single Claims
+    public <T> T extractClaim (String token, Function<Claims,T> claimsResolver){
+        final Claims claims=extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    //Extract Many Claims
+  private Claims extractAllClaims(String token){
+        //Extratc all claims of that specific token
+        return Jwts.parser().
+                setSigningKey(getSignInkey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInkey() {
+        byte[] keyBytes= Base64.getDecoder().decode(SECRET_KEY);
+        return new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
 }
